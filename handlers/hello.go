71a1672d9191e4295c321a1067cc7597fd54b307
Tella Abdulrasheed <rashtell2@gmail.com>
2022-10-13 13:48:31 +0100

    sever mux and graceful shutdown

---
+++ handlers/hello.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello{
+	return &Hello{l}
+}
+
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r*http.Request){
+	h.l.Println("Hello world")
+
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		// rw.WriteHeader(http.StatusBadRequest)
+		// rw.Write([]byte("Ooops"))
+
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "Hello: %s", d)	
+}
--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"rashtell/micro-service/handlers"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Default handler")
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			// rw.WriteHeader(http.StatusBadRequest)
-			// rw.Write([]byte("Ooops"))
-
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(rw, "hello: %s", d)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.ListenAndServe(":9090", nil)
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
